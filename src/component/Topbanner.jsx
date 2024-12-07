c7c1195759d68e74b2d1c5515bc6fe240bda6a38
Gourav575 <gourav6568@gmail.com>
2024-12-07 14:02:23 +0530

    blog-error

--- src/component/Topbanner.jsx
+++ src/component/Topbanner.jsx
@@ -6,7 +6,7 @@ export default function Topbanner(props) {
     <>
     <div className="w-full banner-img relative">
     <img src={props.banner} alt="" className='w-full' />
-    <h1 className='absolute top-[45%] lg:left-[10%] md:left-[5%] left-[3%] text-white lg:text-5xl md:text-3xl text-xl lg:w-6/12'>{props.heading.name}</h1>
+    {/* <h1 className='absolute top-[45%] lg:left-[10%] md:left-[5%] left-[3%] text-white lg:text-5xl md:text-3xl text-xl lg:w-6/12'>{props.heading.name}</h1> */}
     </div>
     
     </>
